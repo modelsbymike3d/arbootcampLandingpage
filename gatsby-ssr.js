a7a7aa4361573896adb8c84a648a95abef74f810
Michael Porter <michael.porter@utsouthwestern.edu>
2020-10-20 09:29:47 -0500

    trying different gatsby-ssr

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,17 +1,38 @@
 var React = require("react")
+const { Helmet } = require("react-helmet")
 
-// Hack, to reorder the helmet components as first in <head> tag
-exports.onPreRenderHTML = ({ getHeadComponents, replaceHeadComponents }) => {
-  /**
-   * @type {any[]} headComponents
-   */
+export const onRenderBody = ({
+  setHeadComponents,
+  setHtmlAttributes,
+  setBodyAttributes,
+}) => {
+  const helmet = Helmet.renderStatic()
+  setHtmlAttributes(helmet.htmlAttributes.toComponent())
+  setBodyAttributes(helmet.bodyAttributes.toComponent())
+  setHeadComponents([
+    helmet.title.toComponent(),
+    helmet.base.toComponent(),
+    helmet.meta.toComponent(),
+    helmet.link.toComponent(),
+    helmet.noscript.toComponent(),
+    helmet.script.toComponent(),
+    helmet.style.toComponent(),
+  ])
+}
+
+export const onPreRenderHTML = ({
+  getHeadComponents,
+  replaceHeadComponents,
+}) => {
   const headComponents = getHeadComponents()
+  const order = ["title", "base", "meta", "link", "noscript", "script", "style"]
 
-  headComponents.sort((a, b) => {
-    if (a.props && a.props["data-react-helmet"]) {
-      return 0
-    }
-    return 1
-  })
-  replaceHeadComponents(headComponents)
+  const sortedHeadComponents = headComponents
+    .slice(0)
+    .flat()
+    .sort((x, y) => {
+      return order.indexOf(x.type) - order.indexOf(y.type)
+    })
+
+  replaceHeadComponents(sortedHeadComponents)
 }
