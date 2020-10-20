1517fef5d856a898741dc26f7864498b79cd12c8
Michael Porter <michael.porter@utsouthwestern.edu>
2020-10-20 09:35:39 -0500

    one last ssr try for today

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,38 +1,16 @@
-var React = require("react")
-const { Helmet } = require("react-helmet")
+export const onPreRenderHTML = ({ getHeadComponents }) => {
+  if (process.env.NODE_ENV !== "production") return
 
-export const onRenderBody = ({
-  setHeadComponents,
-  setHtmlAttributes,
-  setBodyAttributes,
-}) => {
-  const helmet = Helmet.renderStatic()
-  setHtmlAttributes(helmet.htmlAttributes.toComponent())
-  setBodyAttributes(helmet.bodyAttributes.toComponent())
-  setHeadComponents([
-    helmet.title.toComponent(),
-    helmet.base.toComponent(),
-    helmet.meta.toComponent(),
-    helmet.link.toComponent(),
-    helmet.noscript.toComponent(),
-    helmet.script.toComponent(),
-    helmet.style.toComponent(),
-  ])
-}
+  getHeadComponents().forEach(el => {
+    // Remove inline css.
+    if (el.type === "style") {
+      el.type = "link"
+      el.props["href"] = el.props["data-href"]
+      el.props["rel"] = "stylesheet"
+      el.props["type"] = "text/css"
 
-export const onPreRenderHTML = ({
-  getHeadComponents,
-  replaceHeadComponents,
-}) => {
-  const headComponents = getHeadComponents()
-  const order = ["title", "base", "meta", "link", "noscript", "script", "style"]
-
-  const sortedHeadComponents = headComponents
-    .slice(0)
-    .flat()
-    .sort((x, y) => {
-      return order.indexOf(x.type) - order.indexOf(y.type)
-    })
-
-  replaceHeadComponents(sortedHeadComponents)
+      delete el.props["data-href"]
+      delete el.props["dangerouslySetInnerHTML"]
+    }
+  })
 }
