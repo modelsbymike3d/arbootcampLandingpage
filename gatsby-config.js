9f90e7b65d525b2927b999ab082da707d2fbb4e9
Michael Porter <michael.porter@utsouthwestern.edu>
2020-12-07 15:14:29 -0600

    swapping out google analytics for plausible

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,16 +55,9 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-react-helmet`,
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-plausible`,
       options: {
-        // The property ID; the tracking code won't be generated without it
-        trackingId: "UA-173415966-1",
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: true,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
+        domain: `arbootcamp.com`,
       },
     },
   ],
